c1291fb05a346151246b46a3e1cb8b8482939c70
naojitaniguchi <naoji.taniguchi@gmail.com>
2018-06-12 09:42:20 +0900

    first commit .

---
+++ Assets/VRRot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRRot : MonoBehaviour
+{
+    public GameObject target;
+    Vector3 prevForward;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            prevForward = transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Quaternion rotQ = Quaternion.FromToRotation(prevForward, transform.forward );
+            // Debug.Log(transform.forward);
+            // Debug.Log(prevForward);
+            target.transform.rotation = rotQ * target.transform.rotation;
+            prevForward = transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            transform.Rotate(0.5f, 0.0f, 0.0f, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Y))
+        {
+            transform.Rotate(0.0f, 0.5f, 0.0f, Space.World);
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+[![Rotation Sample](http://img.youtube.com/vi/vQZRVLJft3s/0.jpg)](http://www.youtube.com/watch?v=vQZRVLJft3s)
+
+Rotation sample for VR device .
+White Box is supporsed to VR controller .
+Red Box is target to rotate by VR controller .
+
+Push A and X key , Red box rotate around X axis .
+Push A and Y key , Red box rotate around Y axis .
+Push R key , reset rotation of white cube .
